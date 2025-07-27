05d8a2d5053c7640637118a96182c417c837ce96
Saath Satheeshkumar <saathsatheesh@gmail.com>
2025-07-27 18:41:06 +0100

    Add write packages permission to gh token

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -66,7 +66,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/saths008/dropwizard-kodein")
+            url = "https://maven.pkg.github.com/saths008/dropwizard-kodein"
             credentials {
                 username = System.getenv("GITHUB_ACTOR")
                 password = System.getenv("GITHUB_TOKEN")
