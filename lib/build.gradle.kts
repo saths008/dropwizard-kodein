2b4faa3f947f129ef6466f697bef96207c683075
Saath Satheeshkumar <saath.satheeshkumar@pleo.io>
2025-07-28 11:10:23 +0100

    Add java 17 support

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
-java { toolchain { languageVersion = JavaLanguageVersion.of(21) } }
+java { toolchain { languageVersion = JavaLanguageVersion.of(17) } }
 
 tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
